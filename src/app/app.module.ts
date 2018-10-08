03c71e52910d5249f2911dfb5dcc55f7de708407
Huy Pham <huy.pham@objectbrains.com>
2018-10-08 16:20:40 -0700

    Setup: Add home and log components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialImports } from './app.material';
+import { HomeComponent } from './components/home/home.component';
+import { LogComponent } from './components/log/log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
