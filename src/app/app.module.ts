9217f495587eb586839629eeb3308e740fecb561
Huy Pham <huy.pham@objectbrains.com>
2018-10-08 16:19:06 -0700

    Setup: Add toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MaterialImports } from './app.material';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
