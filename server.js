190929179968c3cce60f1e98aa500f614f56f57f
Huy Pham <huy.pham@objectbrains.com>
2018-10-19 14:45:47 -0700

    Setup: Add Access-Control-Allow-Origin and Access-Control-Allow-Headers.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, tokenkey, username");
+  next();
+});
+
 // Routes
 require('./routes/log.routes')(app);
 
 // Render
 const path = require('path');
-app.use(express.static('dist/logger'));
+app.use(express.static('dist/browser'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'logger', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'browser', 'index.html'));
 });
 
 // Start the server
